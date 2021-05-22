930b0b1b82822b862a3d7d2b08fd96244a4cfd7e
George Dialektakis <geo4diale@gmail.com>
2021-05-22 19:40:26 +0300

    Active learning for single label classfication (need to check y in line 89 as it is recognized as Series)

--- src/aml-auth/ranked_batch-mode_sampling.py
+++ src/aml-auth/ranked_batch-mode_sampling.py
@@ -18,33 +18,43 @@ Ranked batch-mode active learning. Information Sciences, Volume 379, 2017, Pages
 https://www.sciencedirect.com/science/article/abs/pii/S0020025516313949
 """
 
+def delete_rows_csr(mat, indices):
+    """
+    Remove the rows denoted by ``indices`` form the CSR sparse matrix ``mat``.
+    """
+    if not isinstance(mat, sp.csr_matrix):
+        raise ValueError("works only for CSR format -- use .tocsr() first")
+    indices = list(indices)
+    mask = np.ones(mat.shape[0], dtype=bool)
+    mask[indices] = False
+    return mat[mask]
+
 def run():
 
     books_df, genres_to_predict = get_fully_processed()
     X = books_df['book_description_processed']
-    y_initial = books_df[genres_to_predict]
+    y_initial = books_df['major_genre']
 
     tfidf_initial = TfidfVectorizer(ngram_range=(1, 2), max_features=5000)
     X_initial = tfidf_initial.fit_transform(X)
 
 
     # Isolate our examples for our labeled dataset.
-    n_labeled_examples = X.shape[0]
-    training_indices = np.random.randint(low=0, high=n_labeled_examples + 1, size=3)
+    n_labeled_examples = X_initial.shape[0]
+    training_indices = np.random.randint(low=0, high=n_labeled_examples + 1, size=10)
 
-    X_train = X[training_indices]
-    y_train = y_initial[training_indices]
+    X_train = X_initial[training_indices, :]
+    y_train = y_initial.iloc[training_indices]
 
     # Isolate the non-training examples we'll be querying.
-    X_pool = np.delete(X, training_indices, axis=0)
-    y_pool = np.delete(y_initial, training_indices, axis=0)
+    X_pool = delete_rows_csr(X_initial, training_indices)
+    y_pool = y_initial.drop(training_indices)
 
-    tfidf = TfidfVectorizer(ngram_range=(1, 2), max_features=5000)
-    X_train = tfidf.fit_transform(X_train)
-    X_pool = tfidf.transform(X_pool)
+    #tfidf = TfidfVectorizer(ngram_range=(1, 2), max_features=5000)
+    #X_train = tfidf.fit_transform(X_train)
+    #X_pool = tfidf.transform(X_pool)
 
     logreg = LogisticRegression(solver='lbfgs', random_state=0, max_iter=300)
-    classifier_chains = ClassifierChain(logreg, order='random', random_state=0)
 
     # Pre-set our batch sampling to retrieve 3 samples at a time.
     BATCH_SIZE = 3
@@ -52,7 +62,7 @@ def run():
 
     # Specify our active learning model.
     learner = ActiveLearner(
-        estimator=classifier_chains,
+        estimator=logreg,
         X_training=X_train,
         y_training=y_train,
         query_strategy=preset_batch
@@ -62,19 +72,21 @@ def run():
     N_RAW_SAMPLES = 20
     N_QUERIES = N_RAW_SAMPLES // BATCH_SIZE
 
-    unqueried_score = learner.score(X_initial, y)
+    unqueried_score = learner.score(X_initial, y_initial)
     performance_history = [unqueried_score]
 
     for index in range(N_QUERIES):
         query_index, query_instance = learner.query(X_pool)
 
         # Teach our ActiveLearner model the record it has requested.
-        X, y = X_pool[query_index], y_pool[query_index]
+        X, y = X_pool[query_index, :], y_pool[query_index]
+        #y = y.to_numpy()
+        y = y.to_frame()
         learner.teach(X=X, y=y)
 
         # Remove the queried instance from the unlabeled pool.
-        X_pool = np.delete(X_pool, query_index, axis=0)
-        y_pool = np.delete(y_pool, query_index)
+        X_pool = delete_rows_csr(X_pool, training_indices)
+        y_pool = y_pool.drop(query_index)
 
         # Calculate and report our model's accuracy.
         model_accuracy = learner.score(X_initial, y_initial)
