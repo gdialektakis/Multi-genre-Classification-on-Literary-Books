aa1539e861e3c87dfcccedefdea560212c5792af
George Dialektakis <geo4diale@gmail.com>
2021-05-24 11:15:34 +0300

    Finilized ranked_batch_mode_sampling.

--- src/aml-auth/ranked_batch_mode_sampling.py
+++ src/aml-auth/ranked_batch_mode_sampling.py
@@ -1,15 +1,11 @@
-import pandas as pd
 import scipy.sparse as sp
-from sklearn import model_selection
 from sklearn.feature_extraction.text import TfidfVectorizer
 from functools import partial
 from modAL.batch import uncertainty_batch_sampling
 from modAL.models import ActiveLearner
-from sklearn.multioutput import ClassifierChain
 from sklearn.linear_model import LogisticRegression
 import numpy as np
 from data_processing import get_fully_processed
-import classifier_chains
 
 
 """ For more information on Ranked batch-mode sampling you can read the following paper: 
@@ -35,24 +31,19 @@ def run():
     X = books_df['book_description_processed']
     y_initial = books_df['major_genre'].values
 
-    tfidf_initial = TfidfVectorizer(ngram_range=(1, 2), max_features=5000)
-    X_initial = tfidf_initial.fit_transform(X)
-
+    tfidf = TfidfVectorizer(ngram_range=(1, 2), max_features=5000)
+    X_initial = tfidf.fit_transform(X)
 
     # Isolate our examples for our labeled dataset.
     n_labeled_examples = X_initial.shape[0]
     training_indices = np.random.randint(low=0, high=n_labeled_examples + 1, size=10)
 
     X_train = X_initial[training_indices, :]
-    y_train = y_initial[training_indices]#y_initial.iloc[training_indices]
+    y_train = y_initial[training_indices]
 
     # Isolate the non-training examples we'll be querying.
     X_pool = delete_rows_csr(X_initial, training_indices)
-    y_pool = np.delete(y_initial, training_indices)#.drop(training_indices)
-
-    #tfidf = TfidfVectorizer(ngram_range=(1, 2), max_features=5000)
-    #X_train = tfidf.fit_transform(X_train)
-    #X_pool = tfidf.transform(X_pool)
+    y_pool = np.delete(y_initial, training_indices)
 
     logreg = LogisticRegression(solver='lbfgs', random_state=0, max_iter=300)
 
@@ -69,10 +60,11 @@ def run():
     )
 
     # Pool-based sampling
-    N_RAW_SAMPLES = 20
+    N_RAW_SAMPLES = 10000
     N_QUERIES = N_RAW_SAMPLES // BATCH_SIZE
 
     unqueried_score = learner.score(X_initial, y_initial)
+    print("Initial Accuracy: ", unqueried_score)
     performance_history = [unqueried_score]
 
     for index in range(N_QUERIES):
@@ -80,13 +72,11 @@ def run():
 
         # Teach our ActiveLearner model the record it has requested.
         X, y = X_pool[query_index, :], y_pool[query_index]
-        #y = y.to_numpy()
-        #y = y.to_frame()
         learner.teach(X=X, y=y)
 
         # Remove the queried instance from the unlabeled pool.
         X_pool = delete_rows_csr(X_pool, query_index)
-        y_pool = np.delete(y_pool, query_index)#y_pool.drop(query_index)
+        y_pool = np.delete(y_pool, query_index)
 
         # Calculate and report our model's accuracy.
         model_accuracy = learner.score(X_initial, y_initial)
