4845569b854b8db7d186044dd929bbafb768a81d
George Dialektakis <geo4diale@gmail.com>
2021-05-25 19:19:56 +0300

    Easy Ensemble and SMOTE. Needs fix

--- src/aml-auth/easy_ensemble.py
+++ src/aml-auth/easy_ensemble.py
@@ -1,12 +1,19 @@
-import scipy.sparse as sp
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
-import numpy as np
+from collections import Counter
+from imblearn.ensemble import EasyEnsembleClassifier
 from data_processing import get_major_genre_split
+from evaluation import evaluate_model
 
 def run():
     X_train, X_test, y_train, y_test = get_major_genre_split()
-    a =1
+
+    print('Original dataset shape %s' % Counter(y_train))
+
+    # resample all classes but the majority class
+    eec = EasyEnsembleClassifier(sampling_strategy='not majority', random_state=42)
+    eec.fit(X_train, y_train)
+    y_pred = eec.predict(X_test)
+
+    evaluate_model(actual=y_test, predicted=y_pred)
 
 
 if __name__ == "__main__":
