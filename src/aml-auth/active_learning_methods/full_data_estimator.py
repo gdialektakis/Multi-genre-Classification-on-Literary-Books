5ba0983bb8ac9e34886944bfc89c461810b64e1d
George Dialektakis <geo4diale@gmail.com>
2021-05-29 21:25:45 +0300

    Completed Active learning code refactoring and also implemented to return number of annotations needed to reached a certain accuracy score

--- src/aml-auth/active_learning_methods/full_data_estimator.py
+++ src/aml-auth/active_learning_methods/full_data_estimator.py
@@ -3,10 +3,12 @@ from sklearn.model_selection import train_test_split
 
 
 def run(X, y, estimator):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
     estimator.fit(X_train, y_train)
     y_pred = estimator.predict(X_test)
 
     accuracy = metrics.accuracy_score(y_test, y_pred)
+    f1_score = metrics.f1_score(y_test, y_pred, average='micro')
     print(f"Accuracy score: {accuracy:.4f}")
+    print(f"F1 score: {f1_score:.4f}")
     return
